6192554681aa4e8c5ff1716c2f935da787ede6ff
Kilian <kiliankoeppchen@googlemail.com>
2013-03-07 11:11:50 +0100

    Add documentation for ToastNotification

--- src/com/example/git/ToastNotification.java
+++ src/com/example/git/ToastNotification.java
@@ -3,21 +3,31 @@ package com.example.git;
 import android.content.Context;
 import android.widget.Toast;
 
+/**
+ * This class provides a toast notification. The notification is displayed
+ *  on top of all other GUI elements. 
+ */
 public class ToastNotification {
 
+	/**
+	 * Creates a new toast notification.
+	 */
 	private ToastNotification() {
 	}
 	
 	/**
-	 * 	Erzeugt einen Toast auf dem Bildschirm.
+	 * 	Show a notification with the given, message for a specific duration for
+	 * 	the application context. The notification isn't destroyed while changing
+	 * 	the activities.
 	 * 
-	 * 	@param message	Nachricht die angezeigt werden soll.
-	 * 	@param duration	Dauer der Nachrichtenanzeige.
+	 * 	@param message	The message that will be displayed.
+	 * 	@param duration	Duration.
+	 *  @param context	The activity context.
 	 */
-	public static void makeToast(String message, int duration, Context mcon) {
-		Context context = mcon.getApplicationContext();
+	public static void makeToast(String message, int duration, Context context) {
+		Context applicationContext = context.getApplicationContext();
 		CharSequence text = message;
-		Toast toast = Toast.makeText(context, text, duration);
+		Toast toast = Toast.makeText(applicationContext, text, duration);
 		toast.show();		
 	}
 }
