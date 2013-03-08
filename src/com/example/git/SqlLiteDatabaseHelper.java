94f2781861b42b8fe00bdcf57b892ad767d8300f
Kilian <kiliankoeppchen@googlemail.com>
2013-03-08 03:15:06 +0100

    Remove unsued code/clean up.

--- src/com/example/git/SqlLiteDatabaseHelper.java
+++ src/com/example/git/SqlLiteDatabaseHelper.java
@@ -1,14 +1,12 @@
 package com.example.git;
 
 import android.content.Context;
-import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 public class SqlLiteDatabaseHelper extends SQLiteOpenHelper {
 	
 		private static SqlLiteDatabaseHelper mInstance;
-		private final Context myContext;
 		private static final String DATABASE_NAME = "woopwoop";
 		private static final int DATABASE_VERSION = 2;
 		private static final String TABLE_NAME = "woop";
@@ -16,7 +14,6 @@ public class SqlLiteDatabaseHelper extends SQLiteOpenHelper {
 		
 		private SqlLiteDatabaseHelper(Context context) {
 			 super(context, DATABASE_NAME, null, DATABASE_VERSION);
-			 this.myContext = context;
 		}
 		
 		public static SqlLiteDatabaseHelper getInstance(Context context) {
@@ -36,34 +33,4 @@ public class SqlLiteDatabaseHelper extends SQLiteOpenHelper {
 	    // TODO Auto-generated method stub
 	    
     }
-		
-		/*
-		 @Override
-		public void close() {
-			super.close();
-			if (myWritableDb != null) {
-				myWritableDb.close();
-				myWritableDb = null;
-			}
-		}
-		
-		public void insertNewRepo(String path) {
-			myWritableDb.execSQL("INSERT INTO " + TABLE_NAME + " (path) VALUES (" +  path + ");");
-		}
-		
-		public void removeRepo(String path) {
-			myWritableDb.execSQL("DELETE FROM " + TABLE_NAME + " WHERE path=" + path + ";");
-		}
-		
-		public Cursor getAllRepos(){
-			String[] columns = new String[]{"path"};
-			Cursor cursor = myWritableDb.query(TABLE_NAME, columns, null, null, null, null, null);
-			return cursor;
-		}
-		public void initDatabase() {
-			if ((myWritableDb == null) || (!myWritableDb.isOpen())) {
-				myWritableDb = this.getWritableDatabase();
-			}
-		}
-		 */
 }
