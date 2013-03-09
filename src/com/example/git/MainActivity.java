9320c8a4242b6df31cecd81d0e1129e968c4c693
Kilian <kiliankoeppchen@googlemail.com>
2013-03-09 03:22:47 +0100

    Move gen ssh keys to the settings menu

--- src/com/example/git/MainActivity.java
+++ src/com/example/git/MainActivity.java
@@ -1,192 +1,92 @@
 package com.example.git;
 
 import android.app.Activity;
-import android.app.AlertDialog;
-import android.content.DialogInterface;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.os.Environment;
-import android.text.InputType;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.Toast;
-
-import com.jcraft.jsch.JSch;
-import com.jcraft.jsch.JSchException;
-import com.jcraft.jsch.KeyPair;
 
 import org.spongycastle.jce.provider.BouncyCastleProvider;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.IOException;
 import java.security.Security;
 
 public class MainActivity extends Activity {
-  
-  static {
-  	BouncyCastleProvider provider = new BouncyCastleProvider();
-  	Security.insertProviderAt(provider, 1);
-  }
-  
+
+	static {
+		BouncyCastleProvider provider = new BouncyCastleProvider();
+		Security.insertProviderAt(provider, 1);
+	}
+
 	final String TAG = getClass().getName();
 	String selectedPath = "";
 
-	
-	//TODO select via intent if only files, dirs or both are allowed to select
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-              
-        Button button_list_repos = (Button) findViewById(R.id.button_list_repos);
-        Button button_init_repo = (Button) findViewById(R.id.button_init_repo);
-        Button button_clone = (Button) findViewById(R.id.button_clone);
-        Button button_genKeys = (Button) findViewById(R.id.button_genKeys);
-        
-        button_list_repos.setOnClickListener(new View.OnClickListener() {
-      		public void onClick(View v) {
-      			Intent intent = new Intent(MainActivity.this, RepositoryListActivity.class);
-      				startActivity(intent);
-      			}
-        });
-        
-        button_genKeys.setOnClickListener(new View.OnClickListener() {
-      		public void onClick(View v) {
-      			SharedPreferences settings = getSharedPreferences(MainActivity.this.getResources().getString(R.string.APPSETTINGS), 0);
-      			String privateKeyFilenameWithPath = settings.getString(MainActivity.this.getResources().getString(R.string.SSHPRIVATEKEYPATHSETTING), "");
-      			String publicKeyFilenameWithPath = settings.getString(MainActivity.this.getResources().getString(R.string.SSHPUBLICKEYPATHSETTING), "");
-      			
-						boolean privateKey = new File(privateKeyFilenameWithPath).isFile();
-						boolean publicKey = new File(publicKeyFilenameWithPath).isFile();
-						if (!privateKey && !publicKey) {
-							AlertDialog.Builder alert = new AlertDialog.Builder(MainActivity.this);                 
-							alert.setTitle("Enter pw");  
-							alert.setMessage("pw");                
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
 
-							final EditText input = new EditText(MainActivity.this); 
-							input.setInputType(InputType.TYPE_TEXT_VARIATION_PASSWORD);
-							alert.setView(input);
+		Button button_list_repos = (Button) findViewById(R.id.button_list_repos);
+		Button button_init_repo = (Button) findViewById(R.id.button_init_repo);
+		Button button_clone = (Button) findViewById(R.id.button_clone);
 
-							alert.setPositiveButton("Ok", new DialogInterface.OnClickListener() {  
-								public void onClick(DialogInterface dialog, int whichButton) {
-									Log.d(TAG,"generate Keys");				
-  								if (generateKeyPair("RSA", Environment.getExternalStorageDirectory() + "/.ssh/", "id_rsa", "",input.getText().toString())) {
-  									ToastNotification.makeToast("Was able to gen keys : )", Toast.LENGTH_LONG, MainActivity.this);
-  								} else {
-  									ToastNotification.makeToast("Wasn't able to gen keys : (", Toast.LENGTH_LONG, MainActivity.this);
-  								}									
-								}
-							});
-      			
-							alert.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
-								public void onClick(DialogInterface dialog, int which) {
-									// TODO Auto-generated method stub
-									return;   
-								}
-							});
-							alert.show();
-						}
-						else {
-							ToastNotification.makeToast("There are keys already. Nothing to do here.", Toast.LENGTH_LONG, MainActivity.this);
-						}
-      		}
-      	});
-        
-        
-        button_clone.setOnClickListener(new View.OnClickListener(){
-      		public void onClick(View v) {
-      			Intent intent = new Intent(MainActivity.this, CloneRepositoryActivity.class);
-    				startActivity(intent);
-  				}
-        });
-        
-        button_init_repo.setOnClickListener(new View.OnClickListener(){
-        		public void onClick(View v) {
-        			Intent intent = new Intent(MainActivity.this, InitRepositoryActivity.class);
-      				startActivity(intent);
-        			}
-        });
-    }   
-    
-		/**
-		 * 
-		 */
-		protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		button_list_repos.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				Intent intent = new Intent(MainActivity.this, RepositoryListActivity.class);
+				startActivity(intent);
+			}
+		});
 
-			if (requestCode == 1) {
-			     if(resultCode == RESULT_OK) {
-			    	 selectedPath = data.getStringExtra("currentPath");
-			    	 ToastNotification.makeToast("Directory " + selectedPath.toString() + " selected!", Toast.LENGTH_LONG, MainActivity.this);	     					      
-			     }
+		button_clone.setOnClickListener(new View.OnClickListener(){
+			public void onClick(View v) {
+				Intent intent = new Intent(MainActivity.this, CloneRepositoryActivity.class);
+				startActivity(intent);
+			}
+		});
 
-			     if (resultCode == RESULT_CANCELED) {
-			    	 //Write your code on no result return 
-			     }
+		button_init_repo.setOnClickListener(new View.OnClickListener(){
+			public void onClick(View v) {
+				Intent intent = new Intent(MainActivity.this, InitRepositoryActivity.class);
+				startActivity(intent);
+			}
+		});
+	}   
+
+	/**
+	 * 
+	 */
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+		if (requestCode == 1) {
+			if(resultCode == RESULT_OK) {
+				selectedPath = data.getStringExtra("currentPath");
+				ToastNotification.makeToast("Directory " + selectedPath.toString() + " selected!", Toast.LENGTH_LONG, MainActivity.this);	     					      
+			}
+
+			if (resultCode == RESULT_CANCELED) {
+				//Write your code on no result return 
 			}
 		}
-		  
-   @Override
-   /**
-    * 
-    */
-   public boolean onCreateOptionsMenu(Menu menu) {
-  	 getMenuInflater().inflate(R.menu.activity_main, menu);
-  	 return true;
-   }  
- 	
-   @Override
-   /**
-    * 
-    */
-   public boolean onOptionsItemSelected (MenuItem item) {
-  	 Intent intent = new Intent(MainActivity.this, SettingsActivity.class);
-  	 startActivity(intent);
-     return true;
-   }
-   
- 	/**
- 	 * 
- 	 * @param keyType
- 	 * @param absolutePath
- 	 * @param filename
- 	 * @param comment
- 	 * @param password
- 	 */
- 	private boolean generateKeyPair(String keyType, String absolutePath, String filename, String comment, String password) {
- 		boolean success = false;
- 	 int type = 0;
- 		if (keyType.equals("DSA")) {
- 			type = KeyPair.DSA;
- 		}
- 		else {
- 			type = KeyPair.RSA;
- 		}
- 	  JSch jsch = new JSch();
- 		KeyPair kpair; 		
- 		try {
- 			kpair = KeyPair.genKeyPair(jsch, type);
- 			kpair.setPassphrase(password);
-	    kpair.writePrivateKey(absolutePath + filename);
-	    kpair.writePublicKey(absolutePath + filename + ".pub", comment);
-	 		Log.d(TAG,"Finger print: " + kpair.getFingerPrint());
-	 		kpair.dispose();
-	 		success = true;
-    } catch (JSchException e) {
-    	Log.e(TAG, "JSchException gen keys");
-	    e.printStackTrace();
-    } catch (FileNotFoundException e) {
-   		Log.e(TAG, "FileNotFoundException gen keys");
-	    e.printStackTrace();
-    } catch (IOException e) {
-    	Log.e(TAG, "IOException gen key");
-	    e.printStackTrace();
-    }
- 	 	return success;
- 	}
+	}
+
+	@Override
+	/**
+	 * 
+	 */
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.activity_main, menu);
+		return true;
+	}  
+
+	@Override
+	/**
+	 * 
+	 */
+	public boolean onOptionsItemSelected (MenuItem item) {
+		Intent intent = new Intent(MainActivity.this, SettingsActivity.class);
+		startActivity(intent);
+		return true;
+	}
 }
--- src/com/example/git/SettingsActivity.java
+++ src/com/example/git/SettingsActivity.java
@@ -1,15 +1,25 @@
 package com.example.git;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.Environment;
+import android.text.InputType;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.jcraft.jsch.JSch;
+import com.jcraft.jsch.JSchException;
+import com.jcraft.jsch.KeyPair;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
 public class SettingsActivity extends Activity {
 
@@ -57,6 +67,42 @@ public class SettingsActivity extends Activity {
 			}
 		});
 
+		Button button_genKeys = (Button) findViewById(R.id.button_genKeys);
+		button_genKeys.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View v) {
+				if (sshPrivateKeyPath == "" && sshPrivateKeyPath == "") {
+					AlertDialog.Builder alert = new AlertDialog.Builder(SettingsActivity.this);                 
+					alert.setTitle("Enter password (optional)");  
+					alert.setMessage("pw");                
+
+					final EditText input = new EditText(SettingsActivity.this); 
+					input.setInputType(InputType.TYPE_TEXT_VARIATION_PASSWORD);
+					alert.setView(input);
+
+					alert.setPositiveButton("Ok", new DialogInterface.OnClickListener() {  
+						public void onClick(DialogInterface dialog, int whichButton) {
+							Log.d(TAG,"generate Keys");				
+							if (generateKeyPair("RSA", Environment.getExternalStorageDirectory() + "/.ssh/", "id_rsa", "", input.getText().toString())) {
+								ToastNotification.makeToast("The keys can be found at " + Environment.getExternalStorageDirectory() + "/.ssh/", Toast.LENGTH_LONG, SettingsActivity.this);
+							} else {
+								ToastNotification.makeToast("Wasn't able to gen keys : (", Toast.LENGTH_LONG, SettingsActivity.this);
+							}									
+						}
+					});
+
+					alert.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+						public void onClick(DialogInterface dialog, int which) {
+							return;   
+						}
+					});
+					alert.show();
+				}
+				else {
+					ToastNotification.makeToast("There are keys already. Nothing to do here.", Toast.LENGTH_LONG, SettingsActivity.this);
+				}
+			}
+		});
+
 		Button saveButton = (Button) findViewById(R.id.button_save);
 		saveButton.setOnClickListener(new View.OnClickListener() {
 			public void onClick(View v) {
@@ -91,5 +137,45 @@ public class SettingsActivity extends Activity {
 				ToastNotification.makeToast("No valid choice!", Toast.LENGTH_LONG, SettingsActivity.this);
 			}
 		}
+	}
+
+	/**
+	 * 
+	 * @param keyType
+	 * @param absolutePath
+	 * @param filename
+	 * @param comment
+	 * @param password
+	 */
+	private boolean generateKeyPair(String keyType, String absolutePath, String filename, String comment, String password) {
+		boolean success = false;
+		int type = 0;
+		if (keyType.equals("DSA")) {
+			type = KeyPair.DSA;
+		}
+		else {
+			type = KeyPair.RSA;
+		}
+		JSch jsch = new JSch();
+		KeyPair kpair; 		
+		try {
+			kpair = KeyPair.genKeyPair(jsch, type);
+			kpair.setPassphrase(password);
+			kpair.writePrivateKey(absolutePath + filename);
+			kpair.writePublicKey(absolutePath + filename + ".pub", comment);
+			Log.d(TAG,"Finger print: " + kpair.getFingerPrint());
+			kpair.dispose();
+			success = true;
+		} catch (JSchException e) {
+			Log.e(TAG, "JSchException gen keys");
+			e.printStackTrace();
+		} catch (FileNotFoundException e) {
+			Log.e(TAG, "FileNotFoundException gen keys");
+			e.printStackTrace();
+		} catch (IOException e) {
+			Log.e(TAG, "IOException gen key");
+			e.printStackTrace();
+		}
+		return success;
 	}
 }
